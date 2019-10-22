d7b83bd36626ddfa36b09b49411e07c007f34d9e
YOJI4910 <fmn.outlook@outlook.com>
2019-10-22 20:17:43 +0900

    Set heroku config

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module AppName
 
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
