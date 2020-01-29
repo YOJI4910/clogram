995208351c00184101f5c7cee60045e702fe6968
YOJI4910 <fmn.outlook@outlook.com>
2020-01-29 18:53:54 +0900

    [add]S3へ保存

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,29 +6,23 @@ class ImageUploader < CarrierWave::Uploader::Base
   # igpostアップロード画像を正方形化
   process resize_to_fill: [1080, 1080, "Center"]
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" +
-  #  [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def extension_whitelist
+    %w(png jpg)
+  end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  def filename
+    original_filename if original_filename
+  end
 end
