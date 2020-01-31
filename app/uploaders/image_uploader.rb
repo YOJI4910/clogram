56ac1bb1d6e18ed4bdb27376a99774a69dcf9e53
YOJI4910 <fmn.outlook@outlook.com>
2020-01-31 15:52:06 +0900

    [add]S3 upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   # igpostアップロード画像を正方形化
   process resize_to_fill: [1080, 1080, "Center"]
 
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
   # アップロードされたファイルを保存するディレクトリをデフォルトに設定する
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
